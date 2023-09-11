2b9eb020e8df6c229de6b9aca8b41a278d6defcc
Fazri Egi Ramadhan <71480242+fazriegi@users.noreply.github.com>
2023-09-11 12:18:47 +0700

    delete light theme & add link in portfolio

--- js/script.js
+++ js/script.js
@@ -27,27 +27,3 @@ window.addEventListener("click", function (e) {
     navMenu.classList.add("hidden");
   }
 });
-
-// dark mode
-const darkToggle = document.querySelector("#dark-toggle");
-const html = document.querySelector("html");
-
-darkToggle.addEventListener("click", function () {
-  if (darkToggle.checked) {
-    html.classList.add("dark");
-    localStorage.theme = "dark";
-  } else {
-    html.classList.remove("dark");
-    localStorage.theme = "light";
-  }
-});
-
-if (
-  localStorage.theme === "dark" ||
-  (!("theme" in localStorage) &&
-    window.matchMedia("(prefers-color-scheme: dark)").matches)
-) {
-  darkToggle.checked = true;
-} else {
-  darkToggle.checked = false;
-}
