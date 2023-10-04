fcffe73cddacc31d2442f061b17626053ef5db39
fazriegi <fazriegi80@gmail.com>
2023-10-04 21:02:20 +0700

    add navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,21 +3,7 @@ module.exports = {
   content: ["index.html"],
   darkMode: "class",
   theme: {
-    container: {
-      center: true,
-      padding: "16px",
-    },
-    extend: {
-      colors: {
-        primary: "#22d3ee",
-      },
-      screens: {
-        "2xl": "1320px",
-      },
-      boxShadow: {
-        dark: "inset 0 -1px 0 0 rgba(255, 255, 255, 0.2);",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
